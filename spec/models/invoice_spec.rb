a83e673f6ee5e7c404342f7bc180768fd751d063
Chris Concannon <concach00@gmail.com>
2016-09-06 19:07:09 -0600

    implemented changes from comments

--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -7,28 +7,9 @@ describe Invoice do
     expect(invoice).to be_valid
   end
   
-  it "is invalid without a customer id" do
-    invoice = FactoryGirl.build(:invoice, customer_id: nil)
-    expect(invoice).to_not be_valid
-  end
-  
-  it "is invalid without a merchant id" do
-    invoice = FactoryGirl.build(:invoice, merchant_id: nil)
-    expect(invoice).to_not be_valid
-  end
-  
-  it "is invalid without a status" do
-    invoice = FactoryGirl.build(:invoice, status: nil)
-    expect(invoice).to_not be_valid
-  end
-  
-  it "is invalid without a creation timestamp" do
-    invoice = FactoryGirl.build(:invoice, created_at: nil)
-    expect(invoice).to_not be_valid
-  end
-  
-  it "is invalid without an update timestamp" do
-    invoice = FactoryGirl.build(:invoice, updated_at: nil)
-    expect(invoice).to_not be_valid
-  end
+  it { should belong_to :customer }
+  it { should belong_to :merchant }
+  it { should validate_presence_of :status }
+  it { should validate_presence_of :created_at }
+  it { should validate_presence_of :updated_at }
 end
