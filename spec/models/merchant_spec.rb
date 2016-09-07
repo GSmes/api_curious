5a53303a675ffc1b777ed093fba0d516623ddbcc
Garrett <garrett.smestad@gmail.com>
2016-09-07 10:17:40 -0600

    Close #23 Create model specs
    - Create Merchant model spec
    - Create Customer model spec
    - Create Transaction model spec
    - Add validations to above listed models
    - Add has_many/belongs_to relationships for Customer-Invoice-Transaction

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Merchant, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "has a valid factory" do
+    invoice = FactoryGirl.create(:merchant)
+
+    expect(invoice).to be_valid
+  end
+
+  it { should have_many :items }
+  it { should have_many :invoices }
+  it { should validate_presence_of :name }
+  it { should validate_presence_of :created_at }
+  it { should validate_presence_of :updated_at }
 end
