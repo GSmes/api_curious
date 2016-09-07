e9b46344597a20051b95f8077db346a04cb0f7c7
Chris Concannon <concach00@gmail.com>
2016-09-06 19:16:47 -0600

    spec passing, commented out matchers with relations that are not yet built

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,7 +1,9 @@
 class Invoice < ActiveRecord::Base
-  belongs_to :customer
-  belongs_to :merchant
+  # belongs_to :customer
+  # belongs_to :merchant
   
+  validates :customer_id, presence: true
+  validates :merchant_id, presence: true
   validates :status, presence: true
   validates :created_at, presence: true
   validates :updated_at, presence: true
