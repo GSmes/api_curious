1a77f996dec0609659a0c1253e5829be053ce49d
Chris Concannon <concach00@gmail.com>
2016-09-07 16:27:24 -0600

    item search controller logic

--- app/controllers/api/v1/items/search_controller.rb
+++ app/controllers/api/v1/items/search_controller.rb
@@ -1,7 +1,27 @@
 class Api::V1::Items::SearchController < ApplicationController
+  respond_to :json
+  
   def index
+    @item = Item.where(item_params)
+    respond_with @item
   end
   
   def show
+    @item = Item.find_by(item_params)
+    respond_with @item
+  end
+  
+  private 
+  
+  def item_params
+    params.require(:item).permit(
+      :id,
+      :name,
+      :description,
+      :unit_price,
+      :merchant_id,
+      :created_at,
+      :updated_at
+    )
   end
 end
