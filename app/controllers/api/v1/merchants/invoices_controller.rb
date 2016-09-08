ff1bd916640d13340104488eea8b3b5de1bacffd
Chris Concannon <concach00@gmail.com>
2016-09-07 19:54:31 -0600

    merchant relations complete

---
+++ app/controllers/api/v1/merchants/invoices_controller.rb
@@ -0,0 +1,8 @@
+class Api::V1::Merchants::InvoicesController < ApplicationController
+  respond_to :json
+  
+  def index
+    @invoices = Invoice.where(merchant_id: params[:id])
+    respond_with @invoices
+  end
+end
