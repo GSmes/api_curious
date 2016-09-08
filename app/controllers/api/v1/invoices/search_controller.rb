121d7f92a7ffd43b3c0802b4bba468a251bfe451
Garrett <garrett.smestad@gmail.com>
2016-09-07 20:43:36 -0600

    Remove .require() from strong params in search controllers
    - Remove .require(:thing) from the strong params in the search controllers for Items, Invoices, and Invoice Items to increase passing tests in spec harness

--- app/controllers/api/v1/invoices/search_controller.rb
+++ app/controllers/api/v1/invoices/search_controller.rb
@@ -1,20 +1,20 @@
 class Api::V1::Invoices::SearchController < ApplicationController
   respond_to :json
-  
+
   def index
     @invoices = Invoice.where(invoice_params)
     respond_with @invoices
   end
-  
+
   def show
     @invoice = Invoice.find_by(invoice_params)
     respond_with @invoice
   end
-  
+
   private
-  
+
   def invoice_params
-    params.require(:invoice).permit(
+    params.permit(
       :id,
       :customer_id,
       :merchant_id,
