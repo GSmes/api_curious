bda34a2f73525ef55259b2c43024a9af0bac4142
Garrett <garrett.smestad@gmail.com>
2016-09-08 23:28:56 -0600

    Implement favorite_merchant BI endpoint for Customers
    - Create favorite_merchant method in Customer model (and spec) to return most frequented merchant
    - Create Customers controller (and spec) for Customers->FavoriteMerchant BI endpoint
    - Update routes to include favorite_merchant BI endpoint for customers
    - Refactor Item model to be less verbose
    - Remove .to_i method from most_items_controller since ActiveRecord apparently handles this automatically

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,9 +1,17 @@
 class Customer < ApplicationRecord
   has_many :invoices
   has_many :transactions, through: :invoices
+  has_many :merchants, through: :invoices
 
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :created_at, presence: true
   validates :updated_at, presence: true
+
+  def favorite_merchant
+    merchants.joins(invoices: :transactions)
+      .where(transactions: {result: "success"})
+      .group(:id)
+      .order("count(invoices.merchant_id) DESC").first
+  end
 end
